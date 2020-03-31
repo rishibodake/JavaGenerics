4be0c79b84c02399c217709c3122880513dcd740
Hrishikesh <39852586+rishibodake@users.noreply.github.com>
2020-03-31 12:00:15 +0530

    Added:Test For Checking Second Position Integer as Max

--- src/test/java/TestMaximumOfThree.java
+++ src/test/java/TestMaximumOfThree.java
@@ -4,11 +4,21 @@ import org.junit.Test;
 public class TestMaximumOfThree {
     //Only object variable creation
     MaximumOfThree objectOfMaximumOfThree;
+    //Test should pass only if first Integer is maximum out of three
     @Test
     public void givenThreeInput_maxIntegerAtFirstPosition_shouldReturnMaximum()
     {
         objectOfMaximumOfThree = new MaximumOfThree(); //Instantiation or object address passing to objectOfMaximumOfThree
-        int checkMaxInteger = objectOfMaximumOfThree.findMaxInteger(5,3,1);
+        int checkMaxInteger = objectOfMaximumOfThree.findMaxInteger(5,3,1); //Here Max Integer is at first position
         Assert.assertEquals(5,checkMaxInteger);
+    }
+
+    //Test should pass only if second Integer is maximum out of three
+    @Test
+    public void givenThreeInput_MaxIntegerAtSecondPosition_shouldReturnMaximum()
+    {
+        objectOfMaximumOfThree = new MaximumOfThree(); //Instantiation or object address passing to objectOfMaximumOfThree
+        int checkMaxInteger = objectOfMaximumOfThree.findMaxInteger(65,110,87); //Here Max Integer is at Second Position
+        Assert.assertEquals(110,checkMaxInteger);
     }
 }
