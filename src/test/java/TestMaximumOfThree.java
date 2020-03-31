6e9f38fa1f4c45ab4ce801254769cd67aa92bdf4
Hrishikesh <39852586+rishibodake@users.noreply.github.com>
2020-03-31 12:31:38 +0530

    Added:Test For Checking Second Position Float as Max

--- src/test/java/TestMaximumOfThree.java
+++ src/test/java/TestMaximumOfThree.java
@@ -39,4 +39,14 @@ public class TestMaximumOfThree {
         float checkFloat = objectOfMaximumOfThree.findMaxFloat(120.45f,45.67f,100.00f); //Here Max Float Number is at first position
         Assert.assertEquals(java.util.Optional.of((Float)120.45f), java.util.Optional.of(checkFloat));
     }
+
+    //Test should pass only if Second Float is Maximum of three
+    @Test
+    public void givenThreeInput_maxFloatAtSecondPosition_shouldReturnMaximumFloatNumber()
+    {
+        objectOfMaximumOfThree = new MaximumOfThree(); //Instantiation or object address passing to objectOfMaximumOfThree
+        float checkFloat = objectOfMaximumOfThree.findMaxFloat(120.45f,450.67f,100.00f); //Here Max Float Number is at Second position
+        Assert.assertEquals(java.util.Optional.of((Float)450.67f), java.util.Optional.of(checkFloat));
+    }
+
 }
