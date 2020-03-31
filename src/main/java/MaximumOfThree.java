625e171d850f80aa358aa3ab2535eebbcc44bfb4
Hrishikesh <39852586+rishibodake@users.noreply.github.com>
2020-03-31 21:34:29 +0530

    Added:Function That Print Result

--- src/main/java/MaximumOfThree.java
+++ src/main/java/MaximumOfThree.java
@@ -1,3 +1,4 @@
+import java.security.spec.RSAOtherPrimeInfo;
 import java.util.Arrays;
 
 public class MaximumOfThree<E extends Comparable<E>> {
@@ -16,6 +17,12 @@ public class MaximumOfThree<E extends Comparable<E>> {
     public <E extends Comparable> E findMaximum(E... inputArray){
         Arrays.sort(inputArray); //Sort the elements in ascending order
         int lastIndexHasMaxElement = inputArray.length-1; //Last Index will hold max value after sort
+        MaximumOfThree.printMax(inputArray[lastIndexHasMaxElement]);
         return inputArray[lastIndexHasMaxElement]; //will return maximum value
+    }
+    //Function that print Result
+    public static <E> void printMax(E maxResult)
+    {
+        System.out.println("Maximum among all input is: "+maxResult);
     }
 }
