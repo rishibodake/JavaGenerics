44a4e5fef9e25f1c528d1e8e45abb940caef56a0
Hrishikesh <39852586+rishibodake@users.noreply.github.com>
2020-03-31 12:12:33 +0530

    Added:Function That Find Maximum of Three Float Number

--- src/main/java/MaximumOfThree.java
+++ src/main/java/MaximumOfThree.java
@@ -1,5 +1,5 @@
 public class MaximumOfThree {
-    //Function to find Maximum Integer of given three 3 different Integers
+    //Function to find Maximum Integer of given 3 different Integers
     public  Integer findMaxInteger(Integer firstInt, Integer secondInt, Integer thirdInt){
         if(firstInt > secondInt && firstInt > thirdInt){
             return firstInt;
@@ -7,6 +7,17 @@ public class MaximumOfThree {
             return secondInt;
         }else {
             return thirdInt;
+        }
+    }
+
+    //Function to find Maximum Floats of given 3 different floats
+    public Float findMaxFloat(Float firstFloat, Float secondFloat, Float thirdFloat){
+        if(firstFloat > secondFloat && firstFloat > thirdFloat){
+            return firstFloat;
+        }else if (secondFloat > firstFloat && secondFloat > thirdFloat){
+            return secondFloat;
+        }else {
+            return thirdFloat;
         }
     }
 }
